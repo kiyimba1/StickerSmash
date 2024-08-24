b3700a93951218cad23a73b5e05390081a5c89b1
Eddie <edwinkiyimba@gmail.com>
2024-08-24 14:56:19 +0300

    Added Action Buttons

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
 import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react';
+import IconButton from './components/IconButton';
+import CircleButton from './components/CircleButton';
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
@@ -11,6 +13,18 @@ export default function App() {
   const [selectedImage, setSelectedImage] = useState(null)
 
   const [showAppOptions, setShowAppOptions] = useState(false)
+
+  const onReset = () =>{
+    setShowAppOptions(false);
+  }
+
+  const onAddStcker = () =>{
+    // TODO implement later
+  }
+
+  const onSaveImageAsync = () => {
+    // TODO implement later
+  }
 
   const pickImageAsync = async()=>{
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -32,7 +46,13 @@ export default function App() {
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       {showAppOptions ? (
-        <View />
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddStcker} />
+            <IconButton icon="save-alt" label="save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
       ):(
         <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
@@ -59,5 +79,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1/3,
     alignItems: 'center'
+  },
+  optionsContainer:{
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow:{
+    alignItems: 'center',
+    flexDirection: 'row',
   }
 });
